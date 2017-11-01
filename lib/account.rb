63476ddd79331cbc51edeafd375f7bfe833c4b58
Francesco <francesco.falchi@gmail.com>
2017-11-01 11:42:42 +0100

    Add expiry method and initialize it

--- lib/account.rb
+++ lib/account.rb
@@ -1,8 +1,17 @@
 class Account
+  require 'date'
   attr_accessor :pin_code, :balance, :account_status, :exp_date, :account_owner
+
+  STANDARD_VALIDITY_YRS = 5
 
   def initialize
     @pin_code = rand(1000..9999)
     @balance = 0
+    @exp_date = set_expire_date
   end
+
+  def set_expire_date
+    Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
+  end
+
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,8 +4,12 @@ describe Account do
   it 'is a four numbers code' do
     expect(subject.pin_code.to_s.length).to eq 4
   end
-  it 'is expected to have a balance of 0 on initialize' do
+  it 'has 0 balance on initialize' do
     expect(subject.balance).to eq 0
+  end
+  it 'is expected to have an expiry date on intialize' do
+    expected_date = Date.today.next_year(5).strftime("%m/%y")
+    expect(subject.exp_date).to eq expected_date
   end
 
 end
