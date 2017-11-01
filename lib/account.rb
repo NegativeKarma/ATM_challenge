0187be62872dd8de727d6bb4a0751c0a32489ecf
Francesco <francesco.falchi@gmail.com>
2017-11-01 12:24:16 +0100

    Add account owner via instance double, add test for instances

--- lib/account.rb
+++ lib/account.rb
@@ -1,15 +1,16 @@
 require 'date'
 
 class Account
-  attr_accessor :pin_code, :balance, :account_status, :exp_date, :account_owner
+  attr_accessor :pin_code, :balance, :account_status, :exp_date, :owner
 
   STANDARD_VALIDITY_YRS = 5
 
-  def initialize
+  def initialize(attrs = {})
     @pin_code = rand(1000..9999)
     @balance = 0
     @exp_date = set_expire_date
     @account_status = :active
+    set_owner(attrs[:owner])
   end
 
   def deactivate
@@ -18,6 +19,16 @@ class Account
 
   def set_expire_date
     Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
+  end
+
+  private
+
+  def set_owner(obj)
+    obj == nil ? missing_owner : @owner = obj
+  end
+
+  def missing_owner
+    raise "Account owner required"
   end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,6 +1,15 @@
 require './lib/account'
 
 describe Account do
+  let(:person) {instance_double('Person', name: 'Johnny')}
+  subject { described_class.new({owner: person}) }
+
+  it 'has an owner' do
+    expect(subject.owner).to eq person
+  end
+  it 'raises error if no owner is set' do
+    expect { described_class.new}.to raise_error 'Account owner required'
+  end
   it 'is a four numbers code' do
     expect(subject.pin_code.to_s.length).to eq 4
   end
