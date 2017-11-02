86a6c45215310997b433c84004b0f47d3ba905f0
Francesco <francesco.falchi@gmail.com>
2017-11-02 14:25:12 +0100

    Add withdraw method and private withdraw_funds method

--- lib/person.rb
+++ lib/person.rb
@@ -17,11 +17,24 @@ class Person
     @account == nil ? no_account : deposit_cash(amount)
   end
 
+  def withdraw(args = {})
+    @account == nil ? no_account : withdraw_funds(args)
+  end
+
   private
 
   def deposit_cash(amount)
     @cash -= amount
     @account.balance += amount
+  end
+
+  def withdraw_funds(args)
+    args[:atm] == nil ? missing_atm : atm = args [:atm]
+    account = @account
+    amount = args[:amount]
+    pin = args [:pin]
+    response = atm.withdraw(amount, pin, account)
+    response[:status] == true ? increase_cash(response) : response
   end
 
   def set_name(name)
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -44,6 +44,10 @@ describe 'can manage funds if an account has been created' do
     expect(subject.account.balance).to be 100
     expect(subject.cash).to be 0
   end
+  it 'can withdraw funds' do
+    command = lambda { subject.withdraw(amount: 100, pin: subject.account.pin_code, account: subject.account, atm: atm) }
+    expect(command.call).to be_truthy
+  end
 end
 
 describe 'can not manage funds if no account has been created' do
